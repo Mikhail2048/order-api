1071a093b457aa09c1a4b5c732ae68e3fa37d27c
Abubakar <muradov.abubakar999@gmail.com>
2022-11-26 21:59:11 +0400

    Доработано

--- src/main/java/ru/zig/orderapi/database/entity/OrderProduct.java
+++ src/main/java/ru/zig/orderapi/database/entity/OrderProduct.java
@@ -26,15 +26,30 @@ public class OrderProduct implements BaseEntity<Long>{
     @JoinColumn(name = "product_id")
     private Product product;
 
-//    public
-
-    public void setOrder(Order order) {
+//TODO если тут какая-то шибка будет, то я возможно Long id пихнул в него неправильно так как он по сути
+// должен сам генерится. В общем разберись с этим если что-то пойдёт не так
+    public OrderProduct(Long id, Order order, Product product) {
+        this.id = id;
         this.order = order;
         this.order.getOrderProducts().add(this);
-    }
 
-    public void setProduct(Product product) {
         this.product = product;
         this.product.getOrderProducts().add(this);
     }
+
+
+    /**
+     *     Чтобы не вызывать самому сеттеры для соотвествия объектной и реляционной модели эти методы
+     *     пихнул в AllArgsConstructor
+     */
+
+//    public void setOrder(Order order) {
+//        this.order = order;
+//        this.order.getOrderProducts().add(this);
+//    }
+//
+//    public void setProduct(Product product) {
+//        this.product = product;
+//        this.product.getOrderProducts().add(this);
+//    }
 }
--- src/main/resources/db/changelog/db.changelog-2.0.sql
+++ src/main/resources/db/changelog/db.changelog-2.0.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS orders_product
 (
     id BIGSERIAL PRIMARY KEY ,
     orders_id BIGINT NOT NULL REFERENCES orders (id) ON DELETE CASCADE ,
-    product_id BIGINT NOT NULL REFERENCES product (id) ON DELETE CASCADE ,
-    UNIQUE (orders_id, product_id)
+    product_id BIGINT NOT NULL REFERENCES product (id) ON DELETE CASCADE
+
 );
